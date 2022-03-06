1147aff5c0e73af0137347b229201c4db95ebb73
Marecek6 <harudam.02@spst.eu>
2022-03-06 19:39:29 +0100

    Metody10

---
+++ Metody10/Cifry.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metody10
+{
+    class Cifry
+    {
+        public static bool obsahujeCif(string a, out int cifSoucet, out int lichSoucet, out int sudSoucet)
+        {
+            bool obsahujeCif = false;
+            cifSoucet = 0;
+            lichSoucet = 0;
+            sudSoucet = 0;
+            for (int i = 0; i < a.Length; ++i)
+            {
+                if (Char.IsNumber(a[i]))
+                {
+                    obsahujeCif = true;
+                    cifSoucet += int.Parse(a[i].ToString());
+
+                    if (int.Parse(a[i].ToString()) % 2 != 0)
+                    {
+                        lichSoucet += int.Parse(a[i].ToString());
+                    }
+                    else if (int.Parse(a[i].ToString()) % 2 == 0)
+                    {
+                        sudSoucet += int.Parse(a[i].ToString());
+                    }
+                }
+            }
+
+            return obsahujeCif;
+        }
+    }
+}
---
+++ Metody10/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Metody10
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string idk = textBox1.Text;
+            int cifSoucet = 0, lichSoucet = 0, sudSoucet = 0;
+            if (Cifry.obsahujeCif(idk, out cifSoucet, out lichSoucet, out sudSoucet))
+            {
+                MessageBox.Show("Obsahuje číslici");
+                MessageBox.Show("Ciferný součet je " + cifSoucet);
+                MessageBox.Show("Ciferný součet lichých čísel je " + lichSoucet);
+                MessageBox.Show("Ciferný součet sudých čísel je " + sudSoucet);
+            }
+            else MessageBox.Show("Neobsahuje cifry");
+        }
+    }
+}
